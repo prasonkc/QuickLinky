05cdda323a3a594bf7554b91b3f34372adaf7714
Prason Kc <prasonkc07@gmail.com>
2025-10-02 09:21:29 +0545

    Footer Creation

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -6,11 +6,11 @@ const Navbar = () => {
     <div>
       <header className="text-gray-400 bg-gray-900 body-font">
         <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-          <a className="flex title-font font-medium items-center text-white mb-4 md:mb-0">
+          <Link href={"/"} className="flex title-font font-medium items-center text-white mb-4 md:mb-0">
             <span className="ml-3 text-xl">ShortURL</span>
-          </a>
+          </Link>
           <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
-            <Link className="mr-5 hover:text-white" href={"/"}>Home</Link>
+            <Link className="mr-5 hover:text-white" href={"/generate"}>Shorten URL</Link>
             <Link className="mr-5 hover:text-white" href={"/about"}>About</Link>
           </nav>
         </div>
