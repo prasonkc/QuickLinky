7494fa6b821feba95290da5fc94c013a017e7507
Prason Kc <prasonkc07@gmail.com>
2025-10-03 21:20:35 +0545

    Update fetch url

--- app/shorten/page.js
+++ app/shorten/page.js
@@ -2,7 +2,7 @@
 
 import { useState } from "react";
 
-const API_URL = "http://localhost:3000/api/generate";
+const API_URL = "https://short-url-eong.vercel.app";
 
 export default function URLShortener() {
   const [longURL, setLongURL] = useState("");
